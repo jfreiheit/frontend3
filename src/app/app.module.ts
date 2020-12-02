dfbec16ad7f6817f1c68546e9901ff22ab80c8b6
J. Freiheit <freiheit@htw-berlin.de>
2020-12-02 15:06:02 +0100

    Übung 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,54 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReadComponent } from './members/read/read.component';
+import { CreateComponent } from './members/create/create.component';
+import { UpdateComponent } from './members/update/update.component';
+import { DeleteComponent } from './members/delete/delete.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { PencilSquare } from 'ngx-bootstrap-icons';
+import { FormComponent } from './members/read/form/form.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {CommonModule} from '@angular/common';
+
+/* siehe https://avmaisak.github.io/ngx-bootstrap-icons/ */
+const icons = {
+  PencilSquare,
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadComponent,
+    CreateComponent,
+    UpdateComponent,
+    DeleteComponent,
+    FormComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxBootstrapIconsModule.pick(icons),
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
